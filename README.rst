da4456b19dc95ff3a967e6f0ee4bf181578c81b5
Ryan Petrello <lists@ryanpetrello.com>
2015-03-19 16:22:34 -0400

    Provision passworded Jenkins + jjb via ansible.

---
+++ README.rst
@@ -0,0 +1,3 @@
+1.  `mv jenkins/playbooks/hosts.example jenkins/playbooks/hosts`
+2.  Update `jenkins/playbooks/hosts` with the target SSH hostname and username.
+3.  `./jenkins/deploy`
---
+++ jenkins/playbooks/templates/jjb/configure.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import os
+import base64
+import hashlib
+from M2Crypto.EVP import Cipher
+
+MAGIC = "::::MAGIC::::"
+
+
+def main():
+    master_key = open('{{jenkins_home}}/secrets/master.key').read()
+    hudson_secret_key = open(
+        '{{jenkins_home}}/secrets/hudson.util.Secret'
+    ).read()
+
+    hashed_master_key = hashlib.sha256(master_key).digest()[:16]
+    cipher = Cipher('aes_128_ecb', hashed_master_key, '', 0)
+    v = cipher.update(hudson_secret_key)
+    x = v + cipher.final()
+    assert MAGIC in x
+
+    k = x[:-16]
+    k = k[:16]
+
+    token = os.urandom(16).encode('hex')
+
+    plaintext = token + MAGIC
+    cipher = Cipher('aes_128_ecb', k, '', 1)
+    v = cipher.update(plaintext)
+    password = base64.b64encode(v + cipher.final())
+    print password
+
+    with open('/etc/jenkins_jobs/jenkins_jobs.ini', 'wb') as f:
+        f.write('\n'.join([
+            '[jenkins]',
+            'user=jenkins',
+            'password=%s' % hashlib.md5(token).hexdigest(),
+            'url=http://localhost:8080'
+        ]))
+
+if __name__ == '__main__':
+    main()
